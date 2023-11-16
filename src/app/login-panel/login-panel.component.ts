983250b15cb458bea6ea6a78519f8da1fbd75e5d
Bohdan <bogdanrizko@gmail.com>
2023-11-16 20:16:30 +0200

    firstCommit

---
+++ src/app/login-panel/login-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-panel.component.html',
+  styleUrl: './login-panel.component.css'
+})
+export class LoginPanelComponent {
+
+}
